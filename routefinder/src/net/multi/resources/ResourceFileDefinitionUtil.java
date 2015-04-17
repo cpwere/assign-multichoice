1fa323f1143320d1021501274c1c967a9de79f26
Siphiwe <cpwere@yahoo.com>
2015-04-17 20:26:06 +0200

    Initial commit , routerfinder java application assign

---
+++ routefinder/src/net/multi/resources/ResourceFileDefinitionUtil.java
@@ -0,0 +1,13 @@
+package net.multi.resources;
+
+public class ResourceFileDefinitionUtil {
+
+	public static final String propertiesFilename = "./terrain.properties";
+	public static final String inputFileName = "./input_large_map.txt";
+	public static final String outputfile = "./large_map.txt";
+	public static final String inputFile_verifytestinputMap = "./verify_testinput_map.txt";
+	public static final String inputFile_testinputMap = "./testinput_map.txt";
+	
+	
+	
+}
